b58a62af96261aac48fbe126804aa617e261a949
iwakura1ain <ernie937@gmail.com>
2023-04-12 15:11:27 +0900

    initial userservice

--- UserService/UserAPI/app.py
+++ UserService/UserAPI/app.py
@@ -1,10 +1,53 @@
 from flask import Flask
-from flask_restx import Resource, Api
-from auth import Auth
+from flask_restx import Resource, Api, Namespace
+from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
-api = Api(
-    app,
+#from auth import AUTH
+import os
+import db
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config["JWT_SECRET_KEY"] = "DEVELOPMENT-KEY-MUST-CHANGE"
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # connect db and init
+    db.init_app(app)
+
+    # # register auth blueprint from auth.py
+    # app.register_blueprint(auth.bp)
+
+    # # register blog blueprint from blog.pycache
+    # from . import blog
+    # app.register_blueprint(blog.bp)
+    # app.add_url_rule('/', endpoint='index')
+
+    # # a simple page that says hello
+    # @app.route('/hello')
+    # def hello():
+    #     return 'Hello, World!'
+
+    return app
+
+
+APP = create_app()
+
+API = Api(
+    APP,
     version='0.1',
     title="API auth test",
     description="API auth test",
@@ -13,7 +56,14 @@ api = Api(
     license="GPL3"
 )
 
-api.add_namespace(Auth, '/auth')
+AUTH = Namespace(
+    name="Auth",
+    description="사용자 인증을 위한 API",
+)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    APP.run(debug=True, host='0.0.0.0', port=5000)
+
+
+
+
---
+++ UserService/UserAPI/db.py
@@ -0,0 +1,104 @@
+import sqlite3
+from flask import g
+
+# DATABASE = 'sqlite3.db'
+
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#     return db
+
+
+# def close_connection(exception):
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
+
+
+import flask
+import json
+import mariadb
+
+import click
+from flask import current_app, g
+
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'testusr2',
+    'password': '1234',
+    'database': 'exampledb2',
+    'autocommit': True
+}
+
+
+def get_db():
+
+    # cur.execute("select * from people")
+    # # serialize results into JSON
+    # row_headers=[x[0] for x in cur.description]
+    # rv = cur.fetchall()
+    # json_data=[]
+    # for result in rv:
+    #     json_data.append(dict(zip(row_headers,result)))
+
+    # # return the results!
+    # return json.dumps(json_data)
+
+    if 'db' not in g:
+        g.db = mariadb.connect(**config)
+    return g.db
+
+def get_cur():
+    if 'cur' not in g:
+        g.cur = get_db().cursor()
+
+    return g.cur
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    cur = get_db()
+    # with current_app.open_resource('schema.sql') as f:
+    #     cur.execute(f.read().decode('utf8'))
+
+    # fd = open(filename, 'r')
+    # sqlFile = fd.read()
+    # fd.close()
+    # sqlCommands = sqlFile.split(';')
+
+    # for command in sqlCommands:
+    #     try:
+    #         if command.strip() != '':
+    #             cursor.execute(command)
+    #     except IOError, msg:
+    #         print "Command skipped: ", msg
+
+    with current_app.open_resource("schema.sql") as f:
+        sqlFile = f.read().decode("utf-8")
+        sqlCommands = sqlFile.split(';')
+
+        for command in sqlCommands:
+            try:
+
+                cur.execute(command)
+            except Exception as e:
+                print(e)
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
