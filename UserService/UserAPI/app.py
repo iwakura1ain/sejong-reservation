5120e294db00717a3bb2ee0f16daed5bda4cf07c
iwakura1ain <ernie937@gmail.com>
2023-04-12 17:35:18 +0900

    initial userservice

--- UserService/UserAPI/app.py
+++ UserService/UserAPI/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restx import Resource, Api, Namespace
 from flask_jwt_extended import JWTManager
 
-
 from auth import auth
 import os
 import db
@@ -29,45 +28,15 @@ def create_app(test_config=None):
     # connect db and init
     db.init_app(app)
 
-    # # register auth blueprint from auth.py
-    # app.register_blueprint(auth.bp)
-
-    # # register blog blueprint from blog.pycache
-    # from . import blog
-    # app.register_blueprint(blog.bp)
-    # app.add_url_rule('/', endpoint='index')
-
-    # # a simple page that says hello
-    # @app.route('/hello')
-    # def hello():
-    #     return 'Hello, World!'
-
     return app
 
 
 app = create_app()
 
-api = Api(
-    app,
-    version='0.1',
-    title="API auth test",
-    description="API auth test",
-    terms_url="/",
-    contact="",
-    license="GPL3"
-)
-
-api.add_namespace(auth, '/auth')
+api = Api(app)
+api.add_namespace(auth, '/auth')  # add endpoints from auth.py
 
 jwt = JWTManager(app)
 
-
-
 if __name__ == "__main__":
-    print(app.url_map)
     app.run(debug=True, host='0.0.0.0', port=5000)
-    
-
-
-
-
