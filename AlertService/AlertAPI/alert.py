19c78d00307438a3f66b9d07a387b27fc6ef245d
iwakura1ain <ernie937@gmail.com>
2023-05-12 15:36:53 +0900

    methodize

--- AlertService/AlertAPI/alert.py
+++ AlertService/AlertAPI/alert.py
@@ -16,19 +16,8 @@ EMAIL = Namespace(
     description="사용자에게 이메일을 보내는 API",
 )
 
-def validate(data):
-    keys = ["sender", "receiver", "text", "title"]
-
-    for k in keys:
-        if k not in data:
-            raise KeyError
-
-    return data
-
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
-sender_email = "ernie937@gmail.com"
-receiver_email = "ernie937@gmail.com"
 password = "gwlvstkadqhqelix"
 
 with open("mail-format.html", mode="r") as f:
@@ -37,36 +26,46 @@ with open("mail-format.html", mode="r") as f:
 
 @EMAIL.route('')
 class EmailSender(Resource):
+    @staticmethod
+    def validate(data):
+        keys = ["sender", "receivers", "text", "title"]
+
+        for k in keys:
+            if k not in data:
+                raise KeyError
+
+        return data
+    
+    @staticmethod
+    def create_message(data):
+        for receiver in data["receivers"]:
+            html = mail_format.format(text=data["text"])
+            text = data["text"]
+
+            message = MIMEMultipart("alternative")
+            message["Subject"] = data["title"]
+            message["From"] = data["sender"]
+            message["To"] = receiver
+
+            message.attach(MIMEText(text, "plain"))
+            message.attach(MIMEText(html, "html"))
+
+            yield data["sender"], data["receiver"], message
+
+    
     def post(self):
-        """
-        {
-        sender:
-        receiver:
-        text:
-        }
-        """
-
         from flask import request
-        
+
         try:
-            req = validate(request.json)
+            req = self.validate(request.json)
 
-            context = ssl.create_default_context()
             with smtplib.SMTP(smtp_server, port) as server:
-                for receiver in req["receiver"]:
-                    html = mail_format.format(text=req["text"])
-                    text = req["text"]
-
-                    message = MIMEMultipart("alternative")
-                    message["Subject"] = req["title"]
-                    message["From"] = req["sender"]
-                    message["To"] = receiver
-                    message.attach(MIMEText(text, "plain"))
-                    message.attach(MIMEText(html, "html"))
-
-                    server.starttls(context=context)
-                    server.login(req["sender"], password)
-                    server.sendmail(req["sender"], receiver, message.as_string())
+                context = ssl.create_default_context()
+                server.starttls(context=context)
+                server.login(req["sender"], password)
+                
+                for sender, receiver, message in self.create_message(req):
+                    server.sendmail(sender, receiver, message.as_string())
 
             return {
                 "status": True,
