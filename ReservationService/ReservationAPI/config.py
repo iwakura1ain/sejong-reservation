5d751e3e0934a5daa029ec0c6692dba33c037165
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2023-05-15 15:35:02 +0900

    Create config.py

---
+++ ReservationService/ReservationAPI/config.py
@@ -0,0 +1,10 @@
+model_config = {
+  "username":"development",
+  "password":"1234",
+  "host":"sejong-reservation-dbservice-1",
+  "port":3306,
+}
+
+api_config = {
+  "get_auth_info":"http://172.18.0.2:5005/auth/jwt-status",
+}
