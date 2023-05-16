7b9f6b4a7991dfef77b0acd392252be0ce7da5d8
Hojin Jang <jhojin7@gmail.com>
2023-05-16 14:16:52 +0900

    organize files

--- ReservationService/ReservationAPI/reservation.py
+++ ReservationService/ReservationAPI/reservation.py
@@ -3,94 +3,15 @@ from flask_restx import Resource, Namespace
 
 from sqlalchemy import select, insert, update, delete, func
 
-import json
-from datetime import date, time, timedelta
-
 from config import model_config, api_config
 from service import Service
-
-def serialize(row):
-    return json.loads(json.dumps(dict(row), default=str))
-
-def is_valid_token(auth_info):
-    """
-    checks if token is valid.
-    """
-    if auth_info["msg"] == "Token has expired" \
-        or auth_info["status"] == False:
-        return False
-    return True
-
-def is_admin(auth_info):
-    """
-    check if admin
-    """
-    # TODO 관리자가 아니더라도 user가 관리하는 방이면 관리자급 조회가능하도록?
-    if auth_info["User"]["type"] == 1:
-        return True
-    return False
-
-def check_time_conflict(conn, Reservation, new_reservation):
-    """
-    checks time conflict by checking for date, room, 
-    and start_time or end_time in between new_reservation
-    """
-    new_start_time = new_reservation["start_time"]
-    new_end_time = new_reservation["end_time"]
-    stmt = (select(Reservation.id, Reservation.reservation_date,
-        Reservation.start_time, Reservation.end_time, Reservation.which_room)
-        .where(Reservation.reservation_date == new_reservation["reservation_date"])
-        .where(Reservation.which_room == new_reservation["which_room"])
-        .filter(func.time(Reservation.start_time).between(new_start_time, new_end_time))
-        .filter(func.time(Reservation.end_time).between(new_start_time, new_end_time)))
-    rows = conn.execute(stmt).mappings().fetchall()
-    return [serialize(row) for row in rows]
-
-def check_start_end_time(new_reservation):
-    """
-    checks validity of reservation's start_time and end_time 
-    - start_time should be earlier than end_time.
-    - reservation should be within open hours.
-    """
-    # check if start < end
-    if new_reservation["start_time"] > new_reservation["end_time"]:
-        return "End time earlier than start time"
-    # check if start_time, end_time within open hours
-    if time.fromisoformat(new_reservation["start_time"]) < time(9, 0) \
-        or time.fromisoformat(new_reservation["end_time"]) > time(18, 0):
-        return "Open hours: 0900~1800"
-    return None
-
-# check date constraints
-def check_date_constraints(auth_info, new_reservation):
-    """
-    checks if user is allowed to make reservation for specified date.
-    - returns None if allowed.
-    - returns a message string if not allowed.
-    """
-    diff = date.today() - date.fromisoformat(new_reservation["reservation_date"])
-    if auth_info["User"]["type"] == 1 or auth_info["User"]["type"] == 2:
-        pass
-    elif auth_info["User"]["type"] == 3 and diff > timedelta(weeks=1):
-        return "User(grad) cannot make reservation for this date"
-    elif auth_info["User"]["type"] == 4 and diff > timedelta(days=2):
-        return "User(undergrad) cannot make reservation for this date"
-    return None
-
-def is_authorized(auth_info, reservation):
-    """
-    check if user is authorized to change reservation
-    - returns True if user is creator of reservation or admin
-    - returns False otherwise
-    """
-    user = auth_info["User"]
-    # user is creator of reservation
-    if user["id"] == reservation["creator_id"]:
-        return True
-    # user is admin
-    if user["type"] == 1:
-        return True
-    return False
+from utils import (
+    serialize,
+    is_valid_token, is_admin, is_authorized,
+    check_start_end_time, 
+    check_date_constraints, 
+    check_time_conflict,
+)
 
 ns = Namespace(
     name="reservation",
