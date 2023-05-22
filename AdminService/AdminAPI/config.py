56cb62dd9ec1efcc2e25b121ff988b963def5939
chowchow0048 <suess98@naver.com>
2023-05-22 12:46:37 +0900

    conflict

--- AdminService/AdminAPI/config.py
+++ AdminService/AdminAPI/config.py
@@ -2,18 +2,17 @@ from os import getenv
 
 # model configuration for orm model
 model_config = {
-    "username": getenv("DB_USERNAME"),
-    "password": getenv("DB_PASSWORD"),
-    "host": getenv("DB_URL"),
-    "port": getenv("DB_PORT"),
-    "database": getenv("DB_NAME")
-    
-    # local setting
-    # "host": "127.0.0.1",
-    # "database": "sejong",
-    # "port": 3306,
-    # "username": "development",
-    # "password": 1234
+    # "username": getenv("DB_USERNAME"),
+    # "password": getenv("DB_PASSWORD"),
+    # "host": getenv("DB_URL"),
+    # "port": getenv("DB_PORT"),
+    # "database": getenv("DB_NAME"),
+
+    "username": "development",
+    "password": 1234,
+    "host": "dbservice",
+    "port": 3306,
+    "database": "sejong"
 }
 
 api_config = {
