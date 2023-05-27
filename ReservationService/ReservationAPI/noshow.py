993aac7fe435adb9b5c8e1631de06e45ad5d3520
Hojin Jang <jhojin7@gmail.com>
2023-05-27 09:19:46 +0900

    modify noshow print statements

--- ReservationService/ReservationAPI/noshow.py
+++ ReservationService/ReservationAPI/noshow.py
@@ -5,6 +5,7 @@ from datetime import date, time, datetime
 
 from config import model_config, api_config
 
+
 def check_room_used(room_id, service=None):
     try:
         with service.query_model("Reservation") as (conn, Reservation):
@@ -12,23 +13,25 @@ def check_room_used(room_id, service=None):
             now_time = datetime.now().time()
 
             stmt = (select(Reservation)
-                # select for a room
-                .where(Reservation.room_id == room_id)
-                # for today
-                .where(Reservation.reservation_date == now_date)
-                # only reservations from the past
-                .where(Reservation.end_time < now_time)
-                # room not used
-                .where(Reservation.room_used == 0)
-            )
+                    # select for a room
+                    .where(Reservation.room_id == room_id)
+                    # for today
+                    .where(Reservation.reservation_date == now_date)
+                    # only reservations from the past
+                    .where(Reservation.end_time < now_time)
+                    # room not used
+                    .where(Reservation.room_used == 0)
+                    )
             rows = conn.execute(stmt).mappings().fetchall()
+            print(rows)
 
             no_show_users = []
             for row in rows:
                 if row["room_used"] == 0:
                     no_show_users.append(row["creator_id"])
-            
-        # TODO: maybe use query_api?
+                    print(
+                        f"no_show user_id: {row['creator_id']}, reservation_id:{row['id']}")
+
         with service.query_model("User") as (conn, User):
             for user_id in no_show_users:
                 stmt = (
@@ -38,26 +41,25 @@ def check_room_used(room_id, service=None):
                 )
                 conn.execute(stmt)
 
-                # # sanity check
-                # row = conn.execute(select(User).where(User.id == user_id))
-                # print(row.mappings().fetchone())
+                # sanity check
+                row = conn.execute(select(User).where(User.id == user_id))
+                print(row.mappings().fetchone())
         return True
 
     except Exception:
         return False
 
-if __name__ == "__main__":
-    model_config = {
-        "username": "development",
-        "password": "1234",
-        "host": "127.0.0.1",
-        "port": 3306,
-        "database": "sejong"
-    }
 
+if __name__ == "__main__":
+    print(datetime.now())
     service = Service(model_config=model_config,
                       api_config=api_config)
 
-    room_ids = [1,2,3]
+    with service.query_model("Room") as (conn, Room):
+        rows = conn.execute(select(Room.id)).mappings().fetchall()
+        room_ids = [row["id"] for row in rows]
+    if not room_ids:
+        print("No rooms exist.")
+        exit()
     for id in room_ids:
-        check_room_used(id,service=service)
+        check_room_used(id, service=service)
