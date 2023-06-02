541164c1b9e02678d735a20954bd17b9657a209f
iwakura1ain <ernie937@gmail.com>
2023-06-02 12:41:36 +0900

    modified deploy script

--- deploy_compose.sh
+++ deploy_compose.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+sudo whoami
+
 echo "========= DEPLOYING =========" | tee Actions-Test/testing.txt 
 
 git pull | tee Actions-Test/testing.txt 
 
-docker-compose build --no-cache | tee Actions-Test/testing.txt 
+sudo docker-compose build --no-cache | tee Actions-Test/testing.txt 
 
-docker-compose up --force-recreate --detach | tee Actions-Test/testing.txt 
+sudo docker-compose up --force-recreate --detach | tee Actions-Test/testing.txt 
 
 echo "========= DEPLOYMENT DONE =========" | tee Actions-Test/testing.txt 
 
